3e7c3e238ad9b11134ed7e72f4c58719aa8dd2e8
Derposoft <viswanath.chadalapaka@gmail.com>
2020-04-14 14:05:54 -0700

    +

---
+++ precache-manifest.dd60ec8f3615ef24920d05a744ad0998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bfef1f4c95f5e78e93d6466345b8fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fae73691279e724614c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18f44eaa2f82c495cf0e",
+    "url": "/static/js/2.4dd97f5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4dd97f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fae73691279e724614c",
+    "url": "/static/js/main.04667447.chunk.js"
+  },
+  {
+    "revision": "3d544166d9ab915d62ab",
+    "url": "/static/js/runtime-main.20af63b0.js"
+  }
+]);
