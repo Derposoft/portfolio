9a45178e8df1ee97368dd90af6edb52a3bc28ebf
derposoft <viswanath.chadalapaka@gmail.com>
2022-05-11 17:26:34 -0700

    +rotating button and readme reader

--- react/src/components/Project/Project.jsx
+++ react/src/components/Project/Project.jsx
@@ -3,30 +3,37 @@ import { useState } from 'react'
 import { Box, Collapse, Fade, Grow, Grid } from '@mui/material'
 import { ArrowDropDownCircleSharp } from '@mui/icons-material'
 import ReactMarkdown from 'react-markdown'
+import axios from 'axios'
 
 function Project(props) {
-    const [open, setOpen] = useState(true)
+    const [readme, setReadme] = useState('')
+    axios.get('https://raw.githubusercontent.com/Derposoft/Derposoft.github.io/master/README.md').then(res => {
+        setReadme(res.data)
+        console.log(readme)
+    })
+    const [open, setOpen] = useState(false)
     var showBig = () => {
         setOpen(open ? false : true)
         console.log('open: ' + open)
     }
-    console.log(ReactMarkdown('./../README.md'))
+    
     return (
         <div onClick={showBig} class='project'>
             <Box>
                 <Grid container>
-                    <Grid item xs={10}>
+                    <Grid item xs={8}>
                         <b class='cardtext'>{props.name}</b>
                         <p class='cardtext'>{props.subtitle}</p>
                     </Grid>
-                    <Grid item xs={2} class='dropdownarrow'>
-                        <ArrowDropDownCircleSharp fontSize='large' />
+                    <Grid item xs={2}>
+                        <div className='rotatebutton'>
+                            <ArrowDropDownCircleSharp fontSize='large' className={(open ? 'turndown' : 'turnup')}/>
+                        </div>
                     </Grid>
                 </Grid>
                 <Collapse in={open}>
                     <p class='cardtext'>
-                        
-                        bottom text test
+                        <ReactMarkdown>{readme}</ReactMarkdown>
                     </p>
                 </Collapse>
             </Box>
--- react/src/navigation/Pages/Portfolio.jsx
+++ react/src/navigation/Pages/Portfolio.jsx
@@ -1,5 +1,6 @@
 import './Page.css'
 import Project from '../../components/Project/Project'
+import { height } from '@mui/system'
 
 function Portfolio(props) {
     const projects = [
@@ -7,10 +8,11 @@ function Portfolio(props) {
             name: 'derposoft.com',
             subtitle: 'The website you\'re on!',
             url: 'https://github.com/Derposoft/Derposoft.github.io'
+            // https://raw.githubusercontent.com/Derposoft/Derposoft.github.io/master/README.md
         }
     ]
     return (
-        <div>
+        <div style={{overflowY: 'auto', height: '100%'}}>
             {projects.map(project => {
                 return <Project {...project}/>
             })}
