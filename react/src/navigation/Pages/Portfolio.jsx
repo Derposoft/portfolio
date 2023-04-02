d289a58a74269a281fc3dea6c751c54c2ad8d2bc
Derposoft <viswanath.chadalapaka@gmail.com>
2023-04-02 15:17:40 -0700

    +website update

--- react/src/navigation/Pages/Portfolio.jsx
+++ react/src/navigation/Pages/Portfolio.jsx
@@ -3,16 +3,7 @@ import Project from '../../components/Project/Project'
 import config from '../../utils/config.json'
 
 function Portfolio(props) {
-    const projectNames = [
-        {name: 'Derposoft.github.io', subtitle: 'the website you\'re on!'},
-        {name: 'crypto_pump_and_dump_with_deep_learning', subtitle: 'a deep learning study in the domain of crypto fraud'},
-        {name: 'plot_hole_detection', subtitle: 'an NLP study in detecting logical fallacies'},
-        {name: 'ai-educator', subtitle: 'AI-based course generation website'},
-        {name: 'discord-chess-bot', subtitle: 'a sassy Discord chess bot'},
-        {name: 'multimodal-algorithm', subtitle: 'AWS SageMaker algorithm for multimodal data'},
-        {name: 'commando', subtitle: 'the first thing I ever wrote: a game written in XNA'},
-        {name: 'chess-visualizer', subtitle: 'a visual tool for analyzing past games'}
-    ]
+    const projectNames = config["projects"]
     
     const projects = projectNames.map(x => {
         return {
