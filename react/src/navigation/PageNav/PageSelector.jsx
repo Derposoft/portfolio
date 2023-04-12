7ad657c5792827a74cf487276ba4acd099cae48e
Derposoft <viswanath.chadalapaka@gmail.com>
2023-04-12 10:53:16 -0700

    fix

--- react/src/navigation/PageNav/PageSelector.jsx
+++ react/src/navigation/PageNav/PageSelector.jsx
@@ -11,16 +11,16 @@ function PageSelector(props) {
     return (
         <div class='selector'>
             {pages.map(x => {
-                return <Link style={linkStyle} href={'#'+x}><p>{x}</p></Link>
+                return <Link style={linkStyle} href={'#' + x}><p>{x}</p></Link>
             })}
-            <Divider style={{borderColor:'gray'}} />
-            <img 
-                src='me_shorthair_blurred_square.jpg' 
+            <Divider style={{ borderColor: 'gray' }} />
+            <img
+                src='me_shorthair_blurred_square.jpg'
                 class='pfp'
                 alt='vchad outside sunny'
             />
             <div class='bottomtext'>
-                <Link style={{...linkStyle/*, position: 'absolute', bottom: '10%'*/}} href='https://github.com/Derposoft/Derposoft.github.io' target='_blank'>view source</Link>
+                <Link style={{ ...linkStyle/*, position: 'absolute', bottom: '10%'*/ }} href='https://github.com/Derposoft/portfolio' target='_blank'>view source</Link>
             </div>
         </div>
     )
