57fe08d2db8fb994eb056cdf1132d549e71d3eb0
Derposoft <viswanath.chadalapaka@gmail.com>
2023-06-09 19:21:48 -0700

    +website update

--- react/src/navigation/PageNav/PageSelector.jsx
+++ react/src/navigation/PageNav/PageSelector.jsx
@@ -14,10 +14,12 @@ function PageSelector(props) {
                 return <Link style={linkStyle} href={'#' + x}><p>{x}</p></Link>
             })}
             <Divider style={{ borderColor: 'gray' }} />
+            {/*<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="viswanath-chadalapaka-910b2013b" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/viswanath-chadalapaka-910b2013b?trk=profile-badge">Viswanath Chadalapaka</a></div>*/}
+
             <img
-                src='me_shorthair_blurred_square.jpg'
+                src='me_florida_beach_square.png'
                 class='pfp'
-                alt='vchad outside sunny'
+                alt='me on beach'
             />
             <div class='bottomtext'>
                 <Link style={{ ...linkStyle/*, position: 'absolute', bottom: '10%'*/ }} href='https://github.com/Derposoft/portfolio' target='_blank'>view source</Link>
